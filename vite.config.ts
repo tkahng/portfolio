15d89aff4df7f6ec4fc4d2c3d01be26f0bd940de
Tchunoo Rhee Kahng <tkahng@gmail.com>
2026-03-30 11:34:31 -0700

    feat: fix tailwind and netlify (#3)

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start' // ← add this
 import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tanstackStart(),
     netlify(), // ← add this (anywhere in the array is fine)
     viteReact(),
